b471855ad5e5ee7e1b9834a04d16ec851b82f3cc
Adnan-Git-Hub <adnanmasih8@gmail.com>
2024-12-04 00:35:43 +0500

    new

---
+++ src/app/apply/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function Error() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Oops Something went wrong !</h1>
+            <br />
+            <h2 className="text-2xl font-bold underline pl-5">This is a general error which i created on apply page</h2>
+        </div>
+    )
+}
---
+++ src/app/apply/page.tsx
@@ -0,0 +1,8 @@
+export default function Apply () {
+    throw new Error
+    return (
+        <div>
+            <h1 className="font-bold underline text-4xl">Apply Page!</h1>
+        </div>
+    )
+}
